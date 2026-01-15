85040bcd95a007f730766691f7f85292ff8ec221
Felipe Mattioli <felipe.mattioli@smartinnovate.com.br>
2026-01-15 22:57:36 +0100

    FEAT: Forcing pipeline again

--- src/Api/Feijuca.Auth.Application/Commands/ClientScopes/AddClientScopesCommandHandler.cs
+++ src/Api/Feijuca.Auth.Application/Commands/ClientScopes/AddClientScopesCommandHandler.cs
@@ -5,25 +5,24 @@ using Feijuca.Auth.Domain.Interfaces;
 using MediatR;
 using Feijuca.Auth.Providers;
 
-namespace Feijuca.Auth.Application.Commands.ClientScopes
+namespace Feijuca.Auth.Application.Commands.ClientScopes;
+
+public class AddClientScopesCommandHandler(IClientScopesRepository clientScopesRepository, ITenantProvider tenantService) : IRequestHandler<AddClientScopesCommand, Result<bool>>
 {
-    public class AddClientScopesCommandHandler(IClientScopesRepository clientScopesRepository, ITenantProvider tenantService) : IRequestHandler<AddClientScopesCommand, Result<bool>>
+    public async Task<Result<bool>> Handle(AddClientScopesCommand command, CancellationToken cancellationToken)
     {
-        public async Task<Result<bool>> Handle(AddClientScopesCommand command, CancellationToken cancellationToken)
+        foreach (var clientScope in command.AddClientScopesRequest)
         {
-            foreach (var clientScope in command.AddClientScopesRequest)
+            var scopeEntity = clientScope.ToClientScopesEntity();
+            var result = await clientScopesRepository.AddClientScopesAsync(scopeEntity, tenantService.Tenant.Name, cancellationToken);
+
+            if (string.IsNullOrEmpty(result))
             {
-                var scopeEntity = clientScope.ToClientScopesEntity();
-                var result = await clientScopesRepository.AddClientScopesAsync(scopeEntity, tenantService.Tenant.Name, cancellationToken);
-
-                if (string.IsNullOrEmpty(result))
-                {
-                    return Result<bool>.Failure(ClientScopesErrors.CreateClientScopesError);
-                }
+                return Result<bool>.Failure(ClientScopesErrors.CreateClientScopesError);
             }
-
-
-            return Result<bool>.Success(true);
         }
+
+
+        return Result<bool>.Success(true);
     }
 }
