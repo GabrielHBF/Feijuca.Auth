b995bd6b67309fca3ef53244079cd4a29c8a3ff6
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2026-02-23 14:12:47 +0100

    FEAT: Forcing pipeline

--- src/Api/Feijuca.Auth.Application/Requests/ClientScopes/AddClientScopesRequest.cs
+++ src/Api/Feijuca.Auth.Application/Requests/ClientScopes/AddClientScopesRequest.cs
@@ -1,4 +1,4 @@
-﻿namespace Feijuca.Auth.Application.Requests.ClientScopes
-{
-    public record AddClientScopesRequest(string Name, string Description, bool IncludeInTokenScope);
-}
+﻿namespace Feijuca.Auth.Application.Requests.ClientScopes;
+
+public record AddClientScopesRequest(string Name, string Description, bool IncludeInTokenScope);
+
