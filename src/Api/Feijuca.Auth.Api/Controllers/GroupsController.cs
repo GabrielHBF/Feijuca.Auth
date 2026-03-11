d026f29301d10b6032b5345a77a1fb8b9f1d6899
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2026-03-11 21:56:10 +0100

    FEAT: Adding new changes

--- src/Api/Feijuca.Auth.Api/Controllers/GroupsController.cs
+++ src/Api/Feijuca.Auth.Api/Controllers/GroupsController.cs
@@ -8,93 +8,93 @@ using Mattioli.Configurations.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Feijuca.Auth.Api.Controllers
+namespace Feijuca.Auth.Api.Controllers;
+
+[Route("api/v1/groups")]
+[ApiController]
+[Authorize]
+public class GroupsController(ICommandMediator commandMediator, IQueryMediator queryMediator) : ControllerBase
 {
-    [Route("api/v1/groups")]
-    [ApiController]
-    [Authorize]
-    public class GroupsController(ICommandMediator commandMediator, IQueryMediator queryMediator) : ControllerBase
+    /// <summary>
+    /// Returns all groups registered in the realm
+    /// </summary>
+    /// <returns>
+    /// A 200 OK status code along with the list of groups if the operation is successful;
+    /// otherwise, a 400 Bad Request status code with an error message, or a 500 Internal Server Error status code if something goes wrong.
+    /// </returns>
+    /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken"/> used to observe cancellation requests for the operation.</param>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [RequiredRole("Feijuca.ApiReader")]
+    public async Task<IActionResult> GetGroups(CancellationToken cancellationToken)
     {
-        /// <summary>
-        /// Returns all groups registered in the realm
-        /// </summary>
-        /// <returns>
-        /// A 200 OK status code along with the list of groups if the operation is successful;
-        /// otherwise, a 400 Bad Request status code with an error message, or a 500 Internal Server Error status code if something goes wrong.
-        /// </returns>
-        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken"/> used to observe cancellation requests for the operation.</param>
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [RequiredRole("Feijuca.ApiReader")]
-        public async Task<IActionResult> GetGroups(CancellationToken cancellationToken)
+        var result = await queryMediator.QueryAsync(new GetAllGroupsQuery(true), cancellationToken);
+
+        if (result.IsSuccess)
         {
-            var result = await queryMediator.QueryAsync(new GetAllGroupsQuery(true), cancellationToken);
-
-            if (result.IsSuccess)
-            {
-                return Ok(result.Data);
-            }
-
-            var responseError = Result<string>.Failure(result.Error);
-            return BadRequest(responseError);
+            return Ok(result.Data);
         }
 
-        /// <summary>
-        /// Deletes an existing group from the specified Keycloak realm.
-        /// </summary>
-        /// <returns>
-        /// A 204 No Content status code if the group was successfully deleted;
-        /// otherwise, a 400 Bad Request status code with an error message, or a 500 Internal Server Error status code if something goes wrong.
-        /// </returns>
-        /// <param name="id">The unique identifier of the group to be deleted.</param>
-        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken"/> used to observe cancellation requests for the operation.</param>
-        [HttpDelete]
-        [Route("{id}", Name = nameof(DeleteGroup))]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [RequiredRole("Feijuca.ApiWriter")]
-        public async Task<IActionResult> DeleteGroup([FromRoute] string id, CancellationToken cancellationToken)
+        var responseError = Result<string>.Failure(result.Error);
+        return BadRequest(responseError);
+    }
+
+    /// <summary>
+    /// Deletes an existing group from the specified Keycloak realm.
+    /// </summary>
+    /// <returns>
+    /// A 204 No Content status code if the group was successfully deleted;
+    /// otherwise, a 400 Bad Request status code with an error message, or a 500 Internal Server Error status code if something goes wrong.
+    /// </returns>
+    /// <param name="id">The unique identifier of the group to be deleted.</param>
+    /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken"/> used to observe cancellation requests for the operation.</param>
+    [HttpDelete]
+    [Route("{id}", Name = nameof(DeleteGroup))]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [RequiredRole("Feijuca.ApiWriter")]
+    public async Task<IActionResult> DeleteGroup([FromRoute] string id, CancellationToken cancellationToken)
+    {
+        var result = await commandMediator.SendAsync(new DeleteGroupCommand(id), cancellationToken);
+
+        if (result.IsSuccess)
         {
-            var result = await commandMediator.SendAsync(new DeleteGroupCommand(id), cancellationToken);
-
-            if (result.IsSuccess)
-            {
-                return NoContent();
-            }
-
-            var responseError = Result<string>.Failure(result.Error);
-            return BadRequest(responseError);
+            return NoContent();
         }
 
-        /// <summary>
-        /// Adds a new group to the specified Keycloak realm.
-        /// </summary>
-        /// <returns>
-        /// A 201 Created status code along with a success message if the group is successfully created;
-        /// otherwise, a 400 Bad Request status code with an error message, or a 500 Internal Server Error status code if something goes wrong.
-        /// </returns>
-        /// <param name="addGroupRequest">An object of type <see cref="T:Feijuca.Auth.Common.Models.AddGroupRequest"/> containing the details of the group to be created.</param>
-        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken"/> used to observe cancellation requests for the operation.</param>
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [RequiredRole("Feijuca.ApiWriter")]
-        public async Task<IActionResult> CreateGroup([FromBody] AddGroupRequest addGroupRequest, CancellationToken cancellationToken)
+        var responseError = Result<string>.Failure(result.Error);
+        return BadRequest(responseError);
+    }
+
+    /// <summary>
+    /// Adds a new group to the specified Keycloak realm.
+    /// </summary>
+    /// <returns>
+    /// A 201 Created status code along with a success message if the group is successfully created;
+    /// otherwise, a 400 Bad Request status code with an error message, or a 500 Internal Server Error status code if something goes wrong.
+    /// </returns>
+    /// <param name="addGroupRequest">An object of type <see cref="T:Feijuca.Auth.Common.Models.AddGroupRequest"/> containing the details of the group to be created.</param>
+    /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken"/> used to observe cancellation requests for the operation.</param>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [RequiredRole("Feijuca.ApiWriter")]
+    public async Task<IActionResult> CreateGroup([FromBody] AddGroupRequest addGroupRequest, CancellationToken cancellationToken)
+    {
+        var result = await commandMediator.SendAsync(new AddGroupCommand(addGroupRequest), cancellationToken);
+
+        if (result.IsSuccess)
         {
-            var result = await commandMediator.SendAsync(new AddGroupCommand(addGroupRequest), cancellationToken);
-
-            if (result.IsSuccess)
-            {
-                var response = Result<string>.Success("Group created successfully");
-                return Created("/createGroup", response);
-            }
-
-            var responseError = Result<string>.Failure(result.Error);
-            return BadRequest(responseError);
+            var response = Result<string>.Success("Group created successfully");
+            return Created("/createGroup", response);
         }
+
+        var responseError = Result<string>.Failure(result.Error);
+        return BadRequest(responseError);
     }
 }
+
